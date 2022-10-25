21bf5788ee0d4d0f4f712d03ddaa42c5c37e3ddb
Sunit <sunit.corneleous25@gmail.com>
2022-10-25 14:05:36 +0600

    get single course

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
+// get single course by id
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+
+  const course = courses.find(course => course.id === id);
+
+  res.send(course);
+});
+
 // app listening
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
